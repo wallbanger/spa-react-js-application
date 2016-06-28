8563fb75527091262cc16161bea1fb1479d05e0c
Eugene Koval <iam.eugenekoval@gmail.com>
2016-06-29 01:57:30 +0300

    create first components

---
+++ src/components/page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Page extends Component {
+
+  render() {
+    const text = 'Site Page';
+
+    return (
+        <div>
+          page
+        </div>
+    );
+  }
+}
+
+export default Page;
